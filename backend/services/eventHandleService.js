c53af35a3ff7967983ab3451e64d50527170f540
fantasia-y <therealfantasia@gmx.de>
2021-01-13 12:31:34 +0100

    broadcast standings to all users on request

--- backend/services/eventHandleService.js
+++ backend/services/eventHandleService.js
@@ -23,7 +23,11 @@ class EventHandleService {
             ws.on('message', async (message) => {
                 if (message === 'standings') {
                     let standings = await this.stadingsService.generateStandings();
-                    ws.send(JSON.stringify(standings));
+                    this.wss.clients.forEach((client) => {
+                        if (client.readyState === WebSocket.OPEN) {
+                            client.send(JSON.stringify(standings));
+                        }
+                    });
                 }
             });
         });
